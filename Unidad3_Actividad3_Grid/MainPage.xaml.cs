4cbc89a93d98e1bccba9b40df084f2242877c988
JLPANTOJAMUNAM <jlpantojam@gmail.com>
2016-09-11 15:47:02 -0500

    Uso de Grid
    Acomoda los controles de la Actividad 2 en un Grid de 2 columnas y 3
    renglones.

---
+++ Unidad3_Actividad3_Grid/MainPage.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Unidad3_Actividad3_Grid
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            if (Cuenta.Text == "") return;
+            if (Clientes.SelectedIndex.ToString() == "0") return;
+            if (Propina.SelectedIndex.ToString() == "0") return;
+
+            decimal vRes1 = 0;
+            decimal vRes2 = 0;
+            Decimal vCta = Convert.ToDecimal(Cuenta.Text);
+            var vPer = (ComboBoxItem)Clientes.SelectedItem;
+            var vPro = (ComboBoxItem)Propina.SelectedItem;
+
+            vRes1 = 1 + (Convert.ToDecimal(vPro.Content) / 100);
+            vRes2 = vCta * vRes1;
+            vRes1 = vRes2 / Convert.ToInt32(vPer.Content);
+            resultado1.Text = "Total: $" + vRes2.ToString("#,###.00");
+            resultado2.Text = "Individual: $" + vRes1.ToString("#,###.00");
+        }
+
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            Cuenta.Text = "";
+            Clientes.SelectedIndex = 0;
+            Propina.SelectedIndex = 0;
+            resultado1.Text = "";
+            resultado2.Text = "";
+        }
+    }
+}
